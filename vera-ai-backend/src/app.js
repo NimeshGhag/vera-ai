2d49d8649e9a035ed372be3833498781ac035eeb
NimeshGhag <ghagnimesh490@gmail.com>
2026-02-03 11:48:19 +0530

    feat: serve frontend from backend for render single service

--- vera-ai-backend/src/app.js
+++ vera-ai-backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 // importing routes
 const authRoutes = require("./routes/auth.routes");
@@ -14,13 +15,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.VITE_FRONTEND_URL,
+    origin: true,
     credentials: true,
   }),
+);
+
+app.use(
+  express.static(path.join(__dirname, "..", "..", "vera-ai-frontend", "dist")),
 );
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "vera-ai-frontend", "dist", "index.html"),
+  );
+});
 
 module.exports = app;
